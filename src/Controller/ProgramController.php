0a36c234235e8b2f5e5ad2ace8ad6367bf09f0da
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2020-11-26 10:26:59 +0100

    added a prefix route in ProgramController

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-    
+/**
+* @Route("/programs/", name="program_")
+*/
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("", name="index")
      */
     public function index(): Response
     {
@@ -19,7 +22,7 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/programs/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="program_show")
+     * @Route("{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
      */
     public function show(int $id): Response
     {
