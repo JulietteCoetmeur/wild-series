7824ea217e7d811f34ae4bbaf97524b9ab8ec335
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2020-11-26 10:24:09 +0100

    quest 5 : add a route for show page

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+    
 class ProgramController extends AbstractController
 {
     /**
@@ -15,6 +15,16 @@ class ProgramController extends AbstractController
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/programs/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="program_show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }
