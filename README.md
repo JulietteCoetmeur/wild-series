0a5d434f00654707ea6179137e9468af0708d4e1
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2020-12-07 16:01:06 +0100

    quest 14

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Quest 10 : https://www.loom.com/share/a2d6d312456a443880bc3318d7b335a7
 Quest 11 : https://www.loom.com/share/cea7a52821f24e3f8d47bed210ca124e 
 Quest 12 : https://www.loom.com/share/5d2d6864c54c49baa980185f6170de40 
 Quest 13 : https://www.loom.com/share/c0fb8e54a78e48ed8f07329016deac2a 
+Quest 14 : https://www.loom.com/share/91a82d4adba34be29d71a4bf1bd77dba 
---
+++ src/Controller/ActorController.php
@@ -0,0 +1,45 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use App\Entity\Actor;
+
+/**
+* @Route("/actors", name="actor_")
+*/
+
+class ActorController extends AbstractController
+{
+    /**
+     * @Route("", name="index")
+     */
+    public function index(): Response
+    {
+        $actors = $this->getDoctrine()
+             ->getRepository(Actor::class)
+             ->findAll();
+
+        return $this->render(
+            'actor/index.html.twig',
+            ['actors' => $actors]
+        );
+    }
+
+    
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(Actor $actor): Response
+    {
+        $programs = $actor->getPrograms();
+
+        return $this->render('actor/show.html.twig', [
+            'actor' => $actor,
+            'programs' => $programs
+        ]);
+    }
+}
